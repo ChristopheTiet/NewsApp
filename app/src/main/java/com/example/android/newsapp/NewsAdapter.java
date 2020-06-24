502271f8a39dee1df425b543e77151ca835273fd
Christophe Tiet <christophe.tiet.dev@gmail.com>
2020-06-24 11:08:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/newsapp/NewsAdapter.java
@@ -0,0 +1,96 @@
+
+package com.example.android.newsapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+
+public class NewsAdapter extends ArrayAdapter<News> {
+
+
+    private static final String LOCATION_SEPARATOR = " of ";
+
+
+    public NewsAdapter(Context context, List<News> news) {
+        super(context, 0, news);
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if there is an existing list item view (called convertView) that we can reuse,
+        // otherwise, if convertView is null, then inflate a new list item layout.
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.news_list_item, parent, false);
+        }
+
+        // Find the News at the given position in the list of News
+        News currentNews = getItem(position);
+
+        String title = currentNews.getTitle();
+        String section = currentNews.getSection();
+        String author = currentNews.getAuthor();
+        String url = currentNews.getUrl();
+
+        TextView titleView = (TextView) listItemView.findViewById(R.id.news_title);
+        titleView.setText(title);
+
+        TextView sectionView = (TextView) listItemView.findViewById(R.id.news_section);
+        sectionView.setText(section);
+
+        TextView authorView = (TextView) listItemView.findViewById(R.id.news_author);
+        authorView.setText(author);
+
+        Date newsDate = null;
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        try {
+            newsDate = format.parse(currentNews.getDate());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        // Find the TextView with view ID date
+        TextView dateView = (TextView) listItemView.findViewById(R.id.date);
+        // Format the date string (i.e. "Mar 3, 1984")
+        String formattedDate = formatDate(newsDate);
+        // Display the date of the current News in that TextView
+        dateView.setText(formattedDate);
+
+        // Find the TextView with view ID time
+        TextView timeView = (TextView) listItemView.findViewById(R.id.time);
+        // Format the time string (i.e. "4:30PM")
+        String formattedTime = formatTime(newsDate);
+        // Display the time of the current News in that TextView
+        timeView.setText(formattedTime);
+
+        // Return the list item view that is now showing the appropriate data
+        return listItemView;
+    }
+
+
+    /**
+     * Return the formatted date string (i.e. "Mar 3, 1984") from a Date object.
+     */
+    private String formatDate(Date dateObject) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("LLL dd, yyyy");
+        return dateFormat.format(dateObject);
+    }
+
+    /**
+     * Return the formatted date string (i.e. "4:30 PM") from a Date object.
+     */
+    private String formatTime(Date dateObject) {
+        SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
+        return timeFormat.format(dateObject);
+    }
+}
